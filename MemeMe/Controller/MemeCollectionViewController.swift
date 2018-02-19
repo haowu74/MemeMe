8f75aa31369e6d16f35021f2b69fc03ec865794f
Hao Wu <hao.wu.au@gmail.com>
2018-02-19 20:41:05 +1100

    Worked on iPhone but crash on iPad

--- MemeMe/Controller/MemeCollectionViewController.swift
+++ MemeMe/Controller/MemeCollectionViewController.swift
@@ -6,4 +6,58 @@
 //  Copyright © 2018 S&J. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class MemeCollectionViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+    
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    
+    let cellHeight: CGFloat = 100.0
+    
+    var memes: [Meme]?
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        memes = appDelegate.memes
+        self.tabBarController?.tabBar.isHidden = false
+        self.collectionView?.reloadData()
+    }
+    
+    // MARK: Collection View Data Source
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if let m = memes {
+            return m.count
+        } else {
+            return 0
+        }
+        
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MemeCollectionViewCell", for: indexPath) as! MemeCollectionViewCell
+        if let m = memes {
+            let meme = m[(indexPath as NSIndexPath).row]
+            
+            // Set the name and image
+            cell.memeImageView?.image = meme.memedImage
+        }
+        return cell
+        
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath:IndexPath) {
+        
+        let detailController = self.storyboard!.instantiateViewController(withIdentifier: "MemeDetailViewController") as! MemeDetailViewController
+        detailController.memeIndex = (indexPath as NSIndexPath).row
+        self.navigationController!.pushViewController(detailController, animated: true)
+
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: cellHeight, height: cellHeight)
+    }
+    
+
+}
---
+++ MemeMe/View/MemeCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  MemeCollectionViewCell.swift
+//  MemeMe
+//
+//  Created by Hao Wu on 15/2/18.
+//  Copyright © 2018 S&J. All rights reserved.
+//
+
+import UIKit
+
+// MARK: - MemeCollectionViewCell: UICollectionViewCell
+
+class MemeCollectionViewCell: UICollectionViewCell {
+    
+    // MARK: Outlets
+    
+    @IBOutlet weak var memeImageView: UIImageView!
+
+}
